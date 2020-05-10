24b73766fbe0a93d7dc85992fc326f41664be4b5
Dhruv Gupta <dhruvg20@gmail.com>
2020-05-10 14:20:58 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This was added"
-echo "Dhruv says hi1"
-echp "Ananya says Hi"
+echo "Dhruv says Hi1"
+echp "Ananya says Hi1"
